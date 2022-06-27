1ad25f20f51b0522a7c45db43efb89e1c6ad4812
bou3108 <bbonche@henix.fr>
2022-06-27 14:41:45 +0200

    edit issuer format (cf spec)

---
+++ src/main/java/fr/ans/psc/model/Constants.java
@@ -0,0 +1,8 @@
+package fr.ans.psc.model;
+
+public class Constants {
+
+    public static final String VIHF_VERSION = "2.0";
+
+    public static final String ISSUER_FORMAT = "urn:oasis:names:tc:SAML:1.1:nameid-format:X509SubjectName";
+}
