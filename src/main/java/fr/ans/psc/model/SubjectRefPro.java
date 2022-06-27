f6620d16768fc6969604e607713effb26302f321
bou3108 <bbonche@henix.fr>
2022-06-27 13:33:29 +0200

    add UserInfos

---
+++ src/main/java/fr/ans/psc/model/SubjectRefPro.java
@@ -0,0 +1,9 @@
+package fr.ans.psc.model;
+
+import java.util.List;
+
+public class SubjectRefPro {
+
+    private String civilCode;
+    private List<Practice> exercices;
+}
---
+++ src/main/java/fr/ans/psc/model/UserInfos.java
@@ -0,0 +1,23 @@
+package fr.ans.psc.model;
+
+import java.util.List;
+
+public class UserInfos {
+    private String activitySector;
+    private String sub;
+    private boolean emailVerified;
+    private String subjectOrganization;
+    private String modeAccesRaison;
+    private String preferredUsername;
+    private String givenName;
+    private String accesregulationMedicale;
+    private String uitVersion;
+    private String palierAuthentification;
+    private SubjectRefPro subjectRefPro;
+    private String subjectOrganizationID;
+    private List<String> subjectRoles;
+    private String psiLocale;
+    private List<AlternativeIdentifier> otherIds;
+    private String subjectNameID;
+    private String familyName;
+}
