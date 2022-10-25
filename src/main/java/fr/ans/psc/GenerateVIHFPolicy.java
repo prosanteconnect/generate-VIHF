030ed5fe864bb1924546ec4eadc85e6eb23b6a9d
pmortas <patrick.mortas.ext@esante.gouv.fr>
2022-10-25 14:48:32 +0200

    insert vihf in body (requestContent)

--- src/main/java/fr/ans/psc/GenerateVIHFPolicy.java
+++ src/main/java/fr/ans/psc/GenerateVIHFPolicy.java
@@ -3,21 +3,40 @@
  */
 package fr.ans.psc;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.ans.psc.exception.WrongWorkSituationKeyException;
 import fr.ans.psc.model.prosanteconnect.UserInfos;
 import io.gravitee.common.http.HttpStatusCode;
+import io.gravitee.common.http.MediaType;
 import io.gravitee.gateway.api.ExecutionContext;
 import io.gravitee.gateway.api.Request;
 import io.gravitee.gateway.api.Response;
+import io.gravitee.gateway.api.buffer.Buffer;
+import io.gravitee.gateway.api.http.stream.TransformableRequestStreamBuilder;
+import io.gravitee.gateway.api.stream.ReadWriteStream;
 import io.gravitee.policy.api.PolicyChain;
 import io.gravitee.policy.api.PolicyResult;
 import io.gravitee.policy.api.annotations.OnRequest;
+import io.gravitee.policy.api.annotations.OnRequestContent;
 import io.gravitee.policy.api.annotations.OnResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
 
 import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.function.Function;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 @SuppressWarnings("unused")
 public class GenerateVIHFPolicy {
@@ -40,23 +59,52 @@ public class GenerateVIHFPolicy {
         this.objectMapper = new ObjectMapper();
     }
 
-    @OnRequest
-    public void onRequest(Request request, Response response, ExecutionContext executionContext, PolicyChain policyChain) throws IOException {
-
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+	@OnRequestContent
+    public ReadWriteStream onRequestContent(Request request, Response response, ExecutionContext executionContext, PolicyChain policyChain) {
+    	return TransformableRequestStreamBuilder
+                .on(request)
+                .chain(policyChain)
+                .contentType(MediaType.APPLICATION_XML)
+                .transform(generate(executionContext, request, response, configuration, policyChain))
+                .build();
+    }
+    
+    private Function<Buffer,Buffer> generate(ExecutionContext executionContext,Request request, Response response, GenerateVIHFPolicyConfiguration configuration, PolicyChain policyChain){
+    	return input -> {
         String payload = (String) executionContext.getAttribute("openid.userinfo.payload");
         String workSituId = request.headers().get("X-Worksituation");
         String ins = request.headers().get("X-insHeader");
 
-        UserInfos userInfos = objectMapper.readValue(payload, UserInfos.class);
+        UserInfos userInfos = null;
+		try {
+			userInfos = this.objectMapper.readValue(payload, UserInfos.class);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 
         VihfBuilder vihfBuilder = new VihfBuilder(userInfos, workSituId, ins, configuration);
+        StringWriter writer = new StringWriter();
 
         try {
             log.info("generating VIHF token...");
             String vihf = vihfBuilder.generateVIHF();
-            // ajouter le jeton VIHF généré au contexte gravitee
-            executionContext.setAttribute("vihf.token.payload", vihf);
-            // enchaîner l'exécution de la policy
+         
+            // ajouter le jeton VIHF généré au body
+            Document body = DocumentBuilderFactory.newInstance().newDocumentBuilder().
+            		parse(new InputSource(new StringReader(new String(input.getBytes()))));
+            Document assertion = DocumentBuilderFactory.newInstance().newDocumentBuilder().
+            		parse(new InputSource(new StringReader(vihf)));
+            Element header = (Element)body.getElementsByTagName("soap:Header");
+            header.appendChild(assertion);
+            DOMSource domSource = new DOMSource(body);
+            
+            StreamResult result = new StreamResult(writer);
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            transformer.transform(domSource, result);
+            //TODO check if necessary
             policyChain.doNext(request, response);
 
         } catch (WrongWorkSituationKeyException e) {
@@ -65,6 +113,8 @@ public class GenerateVIHFPolicy {
             log.error("Something went wrong when generating VIHF token", e);
             policyChain.failWith(PolicyResult.failure("Something went wrong when generating VIHF token"));
         }
+        return Buffer.buffer(writer.toString());
+    	};
 
     }
 
