722d67510c05b4183d6f452c9c00a1a95c5935b0
pmortas <patrick.mortas.ext@esante.gouv.fr>
2022-12-08 17:53:43 +0100

    add nex xsd (addressing) and remove unused package

--- src/main/java/org/w3/_2000/xmldsig_/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.3.0 
-// Voir <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
-// Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2022.06.27 à 11:29:41 AM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.w3.org/2000/09/xmldsig#", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.w3._2000.xmldsig_;
